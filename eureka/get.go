183414ab8f5022a5485aabb3b57601810894cc8f
Theofanis Vassiliou-Gioles <vassiliou@web.de>
2021-02-17 10:52:01 +0100

    Revert "get returns empty structs in case of missing information"
    This reverts commit 681ec26e67c86ad15ac228722281967636e199fa.

--- eureka/get.go
+++ eureka/get.go
@@ -12,7 +12,7 @@ func (c *Client) GetApplications() (*Applications, error) {
 	}
 	var applications *Applications = new(Applications)
 	err = xml.Unmarshal(response.Body, applications)
-	return applications, nil
+	return applications, err
 }
 
 func (c *Client) GetApplication(appId string) (*Application, error) {
@@ -23,8 +23,8 @@ func (c *Client) GetApplication(appId string) (*Application, error) {
 		return nil, err
 	}
 	var application *Application = new(Application)
-	xml.Unmarshal(response.Body, application)
-	return application, nil
+	err = xml.Unmarshal(response.Body, application)
+	return application, err
 }
 
 func (c *Client) GetInstance(appId, instanceId string) (*InstanceInfo, error) {
@@ -36,7 +36,7 @@ func (c *Client) GetInstance(appId, instanceId string) (*InstanceInfo, error) {
 	}
 	var instance *InstanceInfo = new(InstanceInfo)
 	err = xml.Unmarshal(response.Body, instance)
-	return instance, nil
+	return instance, err
 }
 
 func (c *Client) GetVIP(vipId string) (*Applications, error) {
@@ -48,7 +48,7 @@ func (c *Client) GetVIP(vipId string) (*Applications, error) {
 	}
 	var applications *Applications = new(Applications)
 	err = xml.Unmarshal(response.Body, applications)
-	return applications, nil
+	return applications, err
 }
 
 func (c *Client) GetSVIP(svipId string) (*Applications, error) {
@@ -60,5 +60,5 @@ func (c *Client) GetSVIP(svipId string) (*Applications, error) {
 	}
 	var applications *Applications = new(Applications)
 	err = xml.Unmarshal(response.Body, applications)
-	return applications, nil
+	return applications, err
 }
